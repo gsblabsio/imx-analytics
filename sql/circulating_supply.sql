6af47dc298848c9fe8502e7349bd0758ed6f068f
gsblabsio <97762064+gsblabsio@users.noreply.github.com>
2022-01-28 13:53:32 -0800

    sql format

--- sql/circulating_supply.sql
+++ sql/circulating_supply.sql
@@ -11,20 +11,19 @@ SELECT ADDRESS,
 	ROUND(100 * (2E9 - value) / 2E9, 3) AS pct_total
 
 FROM
-	(
 
 -- Destination amount
-SELECT T1.TO AS ADDRESS,
-			SUM(T1.VALUE / 1e18) AS VALUE
-		FROM IMMUTABLE_X."IMXToken_evt_Transfer" T1
-		GROUP BY 1
-		UNION
+(SELECT T1.TO AS ADDRESS,
+ 	SUM(T1.VALUE / 1e18) AS VALUE
+ FROM IMMUTABLE_X."IMXToken_evt_Transfer" T1
+ GROUP BY 1
+ UNION
 
 -- Source amount
 SELECT F1.FROM AS ADDRESS,
-            -SUM(F1.VALUE / 1e18) AS VALUE
-		FROM IMMUTABLE_X."IMXToken_evt_Transfer" F1
-		GROUP BY 1) Z
+ 	-SUM(F1.VALUE / 1e18) AS VALUE
+ FROM IMMUTABLE_X."IMXToken_evt_Transfer" F1
+ GROUP BY 1) Z
 
 WHERE ADDRESS = '\xe1d1ad55254b29b43035937894514d0adbac7aea' -- IMX address
 
