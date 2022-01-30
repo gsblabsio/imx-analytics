0af47345b2bc5ffcd0205870b6b1ff543fae1a50
gsblabsio <cyee@gsblabs.io>
2022-01-30 13:50:45 -0800

    updates

--- README.md
+++ README.md
@@ -5,6 +5,16 @@ This is the code repo for the Immutable X community grant proposal #5: [IMX ecos
 It contains the set of SQL queries we use to parse the Ethereum blockchain and retrieve project statistics for [Immutable X](https://www.immutable.com/).
 
 
+## Nice-to-Have
+
+* Count of IMX hodler segmentation over time
+* % of total supply over time by IMX hodler segmentation
+* Average IMX per wallet
+* Average IMX per wallet over time
+* Metric card for IMX market cap
+* Metric card for IMX fully diluted market cap
+* Marketplace statistics: Gods Unchained, Guild of Guardian Heroes, TikTok Top Moments, etc.
+
 
 ## What is Immutable X ?
 The first Layer 2 for NFTs on Ethereum.
--- sql/top50_wallets.sql
+++ sql/top50_wallets.sql
@@ -1,6 +1,7 @@
 /***
 Author: GSB Labs
 Date written: 2022-01-25
+Last updated: 2022-01-30
 Objective: top 50 IMX wallets
 
 Methodology
@@ -11,47 +12,42 @@ Methodology
 ***/
 
 
-SELECT X.ADDRESS,
-	N.NAME,
-	ROUND(SUM(X.VALUE), 3) AS AMOUNT,
-	ROUND(100 * (SUM(X.VALUE) / 2e9), 3) AS PERCENT_TOTAL -- 2 billion max supply
+SELECT X.ADDRESS as "Address",
+	N.NAME as "Label",
+	ROUND(SUM(X.VALUE), 3) AS "Amount",
+	ROUND(100 * (SUM(X.VALUE) / 2e9), 3) AS "% of Total" -- 2 billion max supply
 
 FROM
+	(
 
 -- Destination amount
-(SELECT T1.TO AS ADDRESS,
- 	SUM(T1.VALUE / 1e18) AS VALUE
- FROM IMMUTABLE_X."IMXToken_evt_Transfer" T1
- GROUP BY 1
- 
- UNION
+SELECT T1.TO AS ADDRESS,
+			SUM(T1.VALUE / 1e18) AS VALUE
+		FROM IMMUTABLE_X."IMXToken_evt_Transfer" T1
+		GROUP BY 1
+		UNION
 
 -- Source amount
 SELECT F1.FROM AS ADDRESS,
- 	-SUM(F1.VALUE / 1e18) AS VALUE
- FROM IMMUTABLE_X."IMXToken_evt_Transfer" F1
-  GROUP BY 1) X
-  
-  
+      -SUM(F1.VALUE / 1e18) AS VALUE
+		FROM IMMUTABLE_X."IMXToken_evt_Transfer" F1
+		GROUP BY 1) X
 LEFT JOIN
 	(SELECT L.ADDRESS,
-	 	L.NAME,
-		L.UPDATED_AT
-	 FROM LABELS."labels" L
-	 
-	 INNER JOIN
-	 
--- Grab address label based on last updated	 
-	 (SELECT ADDRESS,
-		MAX(UPDATED_AT) AS UPDATED_AT
-	  FROM LABELS."labels"
-	  GROUP BY 1) Z ON L.ADDRESS = Z.ADDRESS
-	 AND L.UPDATED_AT = Z.UPDATED_AT) N ON N.ADDRESS = X.ADDRESS
-	  
+			L.NAME,
+			L.UPDATED_AT
+		FROM LABELS."labels" L
+		INNER JOIN
+			(
+
+-- Grab address label based on last updated
+SELECT ADDRESS,
+					MAX(UPDATED_AT) AS UPDATED_AT
+				FROM LABELS."labels"
+				GROUP BY 1) Z ON L.ADDRESS = Z.ADDRESS
+		AND L.UPDATED_AT = Z.UPDATED_AT) N ON N.ADDRESS = X.ADDRESS
 GROUP BY 1,2
 ORDER BY 3 DESC
 LIMIT 50
 
 ;
-
-
